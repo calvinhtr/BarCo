97d6edccb4f0e71c576961c2f0b3018e8b869c7b
Richard Chen <chenpipi@live.com>
2019-01-06 22:47:34 -0500

    Barcode Scanner

---
+++ app/src/main/java/com/example/chenp/barco/BarCodeScanner.java
@@ -0,0 +1,47 @@
+package com.example.chenp.barco;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.SparseArray;
+import android.widget.Toast;
+import com.google.android.gms.vision.barcode.Barcode;
+import java.util.List;
+import info.androidhive.barcode.BarcodeReader;
+
+public class BarCodeScanner extends AppCompatActivity implements BarcodeReader.BarcodeReaderListener {
+
+    private BarcodeReader barcodeReader;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bar_code_scanner);
+        barcodeReader = (BarcodeReader) getSupportFragmentManager().findFragmentById(R.id.barcode_fragment);
+    }
+
+
+    @Override
+    public void onScanned(Barcode barcode) {
+        // play beep sound
+        //barcodeReader.playBeep();
+    }
+
+    @Override
+    public void onScannedMultiple(List<Barcode> list) {
+
+    }
+
+    @Override
+    public void onBitmapScanned(SparseArray<Barcode> sparseArray) {
+
+    }
+
+    @Override
+    public void onScanError(String s) {
+
+    }
+
+    @Override
+    public void onCameraPermissionDenied() {
+        Toast.makeText(getApplicationContext(), "Camera permission denied!", Toast.LENGTH_LONG).show();
+    }
+}
